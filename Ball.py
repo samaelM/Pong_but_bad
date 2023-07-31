f2fba1a98a267e519d3a0cf517e646ba58444514
samaelM <maldonadomael@gmail.com>
2023-07-31 17:24:11 +0200

    one or two changes

--- Ball.py
+++ Ball.py
@@ -32,14 +32,15 @@ class Ball():
         self.sy = 0
 
     def update(self, screen, p, a):
+        self.s = max(-20, min(self.s, 20))
         self.x += self.s
         if self.y + self.sy < 0 or self.y + self.sy > SCREEN_HEIGHT:
             self.sy = -self.sy
         self.y += self.sy
         self.rect = pygame.draw.rect(screen, WHITE, (self.x, self.y, self.h, self.h))
         if self.rect.colliderect(p):
-            self.s = -self.s * 1.03
-            self.sy = abs(self.x + (self.h)//2 - p.x + (p.y)//2) * 0.07
+            self.s = -self.s * 1.05
+            self.sy = abs(self.x + (self.h)//2 - p.x + (p.y)//2) * 0.1
         if self.rect.colliderect(a):
-            self.s = -self.s * 1.03
-            self.sy = (self.x + (self.h)//2 - a.x + (a.y)//2) * -0.07
+            self.s = -self.s * 1.05
+            self.sy = (self.x + (self.h)//2 - a.x + (a.y)//2) * -0.1
